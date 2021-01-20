13f691ed061666c456000efe48e700bb5cd41ed5
tom42 <tom42@users.noreply.github.com>
2021-01-20 20:19:23 +0100

    Put stuff into own methds

--- libgbaic/src/input_file.cpp
+++ libgbaic/src/input_file.cpp
@@ -31,6 +31,36 @@
 namespace libgbaic
 {
 
+static void open_elf(ELFIO::elfio& reader, std::istream& stream)
+{
+	if (!reader.load(stream))
+	{
+		throw std::runtime_error("file is not a valid ELF file");
+	}
+}
+
+static void check_header(ELFIO::elfio& reader)
+{
+	// TODO: check elf header. Probably we want to check (or perhaps not all of them, need to check):
+	// * class (32 bit) (EI_CLASS)							MUST BE ELF32
+	// * little endian  (EI_DATA)							MUST BE LITTLE ENDIAN
+	// * version        (EI_VERSION) (get_elf_version)		MUST BE 1
+	// * osabi          (EI_OSABI)							(probably don't check this; we get 0, which is System V, which is certainly not what the GBA uses)  (OR expect it to be 0)
+	// * EI_ABIVERSION										(would't know its meaning. don't check)																(OR expect it to be 0)
+	// * e_type												MUST BE ET_EXEC (2)
+	// * e_machine											MUST BE 0x28 (ARM)
+	// * e_version											MUST BE 0
+	// TODO: testcode: CHECK THESE FIELDS
+	std::cout << "EI_CLASS:      " << (reader.get_class() == ELFCLASS32 ? "ELF32" : "ELF64") << std::endl;
+	std::cout << "EI_DATA:       " << (reader.get_encoding() == ELFDATA2LSB ? "Little endian" : "Big endian") << std::endl;
+	std::cout << "EI_VERSION:    " << ((int)reader.get_elf_version()) << std::endl;
+	std::cout << "EI_OSABI:      " << ((int)reader.get_os_abi()) << std::endl;
+	std::cout << "EI_ABIVERSION: " << ((int)reader.get_abi_version()) << std::endl;
+	std::cout << "e_type:        " << reader.get_type() << std::endl;
+	std::cout << "e_machine:     " << reader.get_machine() << std::endl;
+	std::cout << "e_version:     " << reader.get_version() << std::endl;
+}
+
 input_file::input_file(const std::filesystem::path& path)
 {
 	// TODO: open the stream as binary. Then load ELF file from stream
@@ -59,29 +89,9 @@ input_file::input_file(std::istream& stream)
 void input_file::load_elf(std::istream& stream)
 {
 	ELFIO::elfio reader;
-	if (!reader.load(stream))
-	{
-		throw std::runtime_error("file is not a valid ELF file");
-	}
 
-	// TODO: check elf header. Probably we want to check (or perhaps not all of them, need to check):
-	// * class (32 bit) (EI_CLASS)							MUST BE ELF32
-	// * little endian  (EI_DATA)							MUST BE LITTLE ENDIAN
-	// * version        (EI_VERSION) (get_elf_version)		MUST BE 1
-	// * osabi          (EI_OSABI)							(probably don't check this; we get 0, which is System V, which is certainly not what the GBA uses)  (OR expect it to be 0)
-	// * EI_ABIVERSION										(would't know its meaning. don't check)																(OR expect it to be 0)
-	// * e_type												MUST BE ET_EXEC (2)
-	// * e_machine											MUST BE 0x28 (ARM)
-	// * e_version											MUST BE 0
-	// TODO: testcode: CHECK THESE FIELDS
-	std::cout << "EI_CLASS:      " << (reader.get_class() == ELFCLASS32 ? "ELF32" : "ELF64") << std::endl;
-	std::cout << "EI_DATA:       " << (reader.get_encoding() == ELFDATA2LSB ? "Little endian" : "Big endian") << std::endl;
-	std::cout << "EI_VERSION:    " << ((int)reader.get_elf_version()) << std::endl;
-	std::cout << "EI_OSABI:      " << ((int)reader.get_os_abi()) << std::endl;
-	std::cout << "EI_ABIVERSION: " << ((int)reader.get_abi_version()) << std::endl;
-	std::cout << "e_type:        " << reader.get_type() << std::endl;
-	std::cout << "e_machine:     " << reader.get_machine() << std::endl;
-	std::cout << "e_version:     " << reader.get_version() << std::endl;
+	open_elf(reader, stream);
+	check_header(reader);
 }
 
 }
